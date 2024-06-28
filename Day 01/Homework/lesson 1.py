c4112deb4b7c8e461597baeee62c640c48f5142a
nikoloz-lab <baturinin@gmail.com>
2024-06-28 16:00:21 +0400

    first commit

---
+++ Day 01/Homework/lesson 1.py
@@ -0,0 +1,34 @@
+name = "nikolozi"
+
+
+
+
+surname = "baturini"
+
+
+print(name) 
+
+name = "nikolozi"
+age= 13
+height=179.5  
+
+
+knows_programming = True 
+is_ugly = False 
+
+isgly = False
+
+
+print(name + " " + surname ) 
+
+print("name + age")
+
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming)) 
+
+print(name + " " + str(age))
+
