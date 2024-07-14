87e043144d258e011e6b5bb5d6252d537f15a7e0
nikoloz-lab <baturinin@gmail.com>
2024-07-14 14:18:27 +0400

    commit

---
+++ Day 03/Classwork/classwork 3.py
@@ -0,0 +1,13 @@
+name= nikoloz #it is with out ""
+age=13 
+print age #it is with out () 
+
+surname = "baturini" 
+print (SURNAME) #it is wrothen with a large leters 
+
+
+age 11 #it is with out a = 
+
+"is nikoloz its not completed"  
+
+
---
+++ Day 04/Homework/task2.py
@@ -0,0 +1,13 @@
+"""დაწერეთ 5 არასწორი კოდი და კომენტარების მეშვეობით ახსენით, რატომ არის არასწორი."""
+
+name= nikoloz #it is with out ""
+age=13 
+print age #it is with out () 
+
+surname = "baturini" 
+print (SURNAME) #it is wrothen with a large leters 
+
+
+age 11 #it is with out a = 
+
+"is nikoloz its not completed"  
