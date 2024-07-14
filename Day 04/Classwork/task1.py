4d01692b39416a2f10bc703e29fd042de81cff8d
nikoloz-lab <baturinin@gmail.com>
2024-07-14 14:25:32 +0400

    commit

---
+++ Day 04/Classwork/task1.py
@@ -0,0 +1,11 @@
+name= ledi #it is with out ""
+age=13 
+print age #it is with out () 
+
+surname = "baturini" 
+print (SURNAME) #it is wrothen with a large leters 
+
+
+age 41 #it is with out a = 
+
+"is ledi its not completed"  
