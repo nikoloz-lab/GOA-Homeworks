7ec38b922e7c9dd8bc47fb97285c6c101b7b1657
nikoloz-lab <baturinin@gmail.com>
2024-07-28 12:12:19 +0400

    commit

---
+++ Day 10/Homework/task1.py
@@ -0,0 +1,24 @@
+one = 9>7
+two = 10>3 
+three = 13<15 
+four = 9<7 
+print(one and two)
+print(two and four) 
+print(two and one) 
+print(four and three) 
+print(two and three)
+print(one and two)
+print(two and two)
+print(one and one)
+print(four and four)
+print (three and three)
+print(one and two)
+print(four and three)
+print(two and one)
+print(three and four)
+print (two and three)
+print (one and four) 
+print (two and two)
+print(one and one )
+print(four and four)
+print(three and three)
