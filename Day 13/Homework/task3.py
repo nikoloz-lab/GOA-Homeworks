0b9675a40c804d1d311146de3b056989988e0938
nikoloz-lab <baturinin@gmail.com>
2024-08-11 15:59:49 +0400

    commit

---
+++ Day 13/Homework/task3.py
@@ -0,0 +1,4 @@
+#for loop-ის გამოყენებით დაპრინტეთ 'GOA Best' 10-ჯერ.
+
+for i in range(10):
+    print("GOA Best") 
---
+++ Day 13/Homework/task4.py
@@ -0,0 +1,6 @@
+#Input()-ის გამოყენებით მომხმარებელს შემოატანინეთ მისი სახელი, შემდეგ for loop-ით კი დაპრინტეთ "hello (მომხმარებლის სახელი)" 5 ჯერ.
+
+lizi =  (input("what's frist name you gona add?" ))
+
+for i in range(5):
+    print("hello + liziz ")
