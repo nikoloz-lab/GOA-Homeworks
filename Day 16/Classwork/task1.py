b0e0b75704be5a592258068f76af7a9daec66029
nikoloz-lab <baturinin@gmail.com>
2024-08-18 09:39:31 +0400

    commit

---
+++ Day 16/Classwork/task1.py
@@ -0,0 +1,23 @@
+#while loop-ის მეშვეობით გამოიტანეთ ციფრები 0-დან 10-მდე.
+i = 1
+while i <= 10:
+    print(i)
+    i= 1+1 
+
+#while loop-ის მეშვეობით გამოიტანეთ ციფრები 20-დან 10-მდე.
+k=20
+while k >=18:
+    print (k)
+    k=k-1
+
+#while loop-ის მეშვეობით გამოიტანეთ 'goa' 5-ჯერ.
+x=0
+while x !=5:
+    print("goa")
+
+#მომხმარებელს შემოატანინეთ საიდუმლო სიტყვა და თუ ის დაემთხვა თქვენს საიდუმლო სიტყვას, დაუპრინტეთ 'match'.
+    secret ="hi"
+    secret=input("guess the word")
+    while secret !=secret:
+        secret=input("guess the word")
+
---
+++ Day 16/Homework/task1.py
@@ -0,0 +1,57 @@
+#while loopის გამოყენებით გამოიტანეთ 1-დან 10-მდე რიცხვების ჯამი.
+
+i = 1
+while i <= 10:
+    print(i)
+    i= i+1 
+
+#loop-ით, გააკეთეთ 5 for loop მაგალთ
+
+for i in range(1, 4):
+    print(i) 
+
+    word = "cat"
+for letter in word:
+    print(letter)
+
+    numbers = [10, 20, 30]
+for num in numbers:
+    print(num)
+
+    names = ["Tom", "Sam", "Sue"]
+for name in names:
+    print(name)
+
+    numbers = [1, 2, 3]
+for num in numbers:
+    print(num * 2)
+
+
+#5 while loop მაგალითი 
+
+i = 1
+while i <= 3:
+    print(i)
+    i += 1
+
+n = 1
+while n <= 3:
+    print(n)
+    n += 1
+
+n = 3
+while n > 0:
+    print(n)
+    n -= 1
+
+    n = 1
+while n != 3:
+    print(n)
+    n += 1
+
+    i = 0
+while i != 3:
+    i += 1
+    print(i)
+
+
