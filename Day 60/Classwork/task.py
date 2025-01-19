0f63838ea982dd0a6f4aae6271d49827ae7ff15c
nikoloz-lab <baturinin@gmail.com>
2025-01-19 17:17:35 +0400

    commit

---
+++ Day 60/Classwork/task.py
@@ -0,0 +1,9 @@
+#შექმნით სია 5 ელემენტით, შემდეგ კი დაპრინტეთ ამ ელემენტების ჯამი, for loop.
+my_list = [10, 20, 30, 40, 50]
+
+total_sum = 0
+
+for number in my_list:
+    total_sum += number
+
+print("The sum of the elements is:", total_sum)
