f1fccb7fe720ba70dd3fdb99c91b4bbaeed9dec0
nikoloz-lab <baturinin@gmail.com>
2025-05-09 22:57:35 +0400

    commit

---
+++ Day93/Claswork/script.java
@@ -0,0 +1,13 @@
+//შექმენით ობიექტი სახელწოდებით class, კუთვნილებებით: studentCount, grade და favSubject. შემდეგ გამოიყენეთ if-else განაცხადი, რათა შეამოწმოთ, არის თუ არა studentCount კუთვნილების მნიშვნელობა 30-ზე მეტი. თუ ასეა, დაბეჭდეთ "very big class", სხვა შემთხვევაში დაბეჭდეთ "Small Class".
+
+const class0 = {
+    studentnumber: 34
+    grade: 10
+    favsubject: "biology"
+};
+
+if (class0.studentnumber > 30){
+    consol.log("very big");
+} else {
+    consol.log("small");
+}
